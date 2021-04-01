2703fab2efb7b441c61aee5933ea70c0950052c8
Sébastien Deleuze <sdeleuze@vmware.com>
2021-04-01 16:41:53 +0200

    Upgrade versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,10 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "2.4.0"
+	id("org.springframework.boot") version "2.4.4"
 	id("io.spring.dependency-management") version "1.0.9.RELEASE"
-	kotlin("jvm") version "1.4.20"
-	kotlin("plugin.spring") version "1.4.20"
+	kotlin("jvm") version "1.4.32"
+	kotlin("plugin.spring") version "1.4.32"
 }
 
 group = "com.example"
